c61413442118a0cfc33581b04e8631cee9eae8c5
SimonR99 <simonroy99@hotmail.ca>
2022-07-04 20:59:22 -0400

    first commit

---
+++ src/erbi_nlp/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'erbi_nlp'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Simon Roy',
+    maintainer_email='simonroy99@hotmail.ca',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "cv_command = erbi_nlp.nlp_service:main",
+        ],
+    },
+)
